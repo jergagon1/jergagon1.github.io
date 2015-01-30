55d3e41ed3186b04d7476ede2ca7177558c9714a
Jeremy Gagon <jergagon1@gmail.com>
2015-01-29 22:04:13 -0800

    Update blog content

---
+++ scripts/quote.js
@@ -0,0 +1,3 @@
+//Update the quote on multiple pages
+document.getElementById("quote").getElementsByTagName("q")[0].innerHTML = "Software is a great combination between artistry and engineering.";
+document.getElementById("quote").getElementsByTagName("p")[0].innerHTML = "Bill Gates"
